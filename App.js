40056009f6bd1dff181ac7944cc1ddbbd5afb335
Greg Lim <limjunqu@gmail.com>
2020-06-22 10:13:30 +0800

    Initial commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Container } from 'native-base';
+import MyForm from './MyForm';
+
+export default function App() {
+  return (
+    <Container>
+      <MyForm />      
+    </Container>
+  );
+}
